3fadaf859c169aff764b7ab3f4113d39eeaca017
fhareed1 <folorunshofareed0@gmail.com>
2024-05-23 04:06:26 +0100

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
 import { PiLinkedinLogoBold } from "react-icons/pi";
 import { FiGithub } from "react-icons/fi";
 import { BsTwitterX } from "react-icons/bs";
-import { useState } from "react";
 
 const Footer = () => {
-  const [date, setDate] = useState(new Date().getFullYear());
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <footer className=" bg-[#2d2e32] w-full px-[40px] sm:px-[70px]  py-[5rem] ">
         <div className="m-auto max-w-[107rem] ">
           <div className="items-center flex justify-between flex-col sm:flex-row space-y-5">
             <h3 className="text-[#fff] text-lg whitespace-nowrap">
-              Copyright © {date}. All rights are reserved
+              Copyright © {currentYear}. All rights are reserved
             </h3>
             <div className="flex flex-row items-center gap-4 justify-center lg:justify-normal place-content-center">
               <a
